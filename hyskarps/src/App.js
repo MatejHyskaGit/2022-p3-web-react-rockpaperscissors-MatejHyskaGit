cc9019256323f2a3f26a9926d0ec1c4490126bd5
Matěj Hyška <matej.hyska.020@pslib.cz>
2023-05-02 23:34:24 +0200

    first commit

---
+++ hyskarps/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  //vars
+  const [pcChoice, setPcChoice] = useState(null)
+  const [playerChoice, setPlayerChoice] = useState(null)
+  const [roundCnt, setRoundCnt] = useState(0)
+  const [winCnt, setWinCnt] = useState(0)
+
+//actions
+
+
+
+
+
+//"html"
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
